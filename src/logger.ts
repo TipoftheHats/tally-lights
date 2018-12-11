9436e5c69aca644090c0284d00d0eda4ed0d2bc4
Alex Van Camp <vancamp.alex@gmail.com>
2018-12-11 11:39:40 -0600

    feat: add file logging

--- src/logger.ts
+++ src/logger.ts
@@ -1,7 +1,15 @@
 'use strict';
 
+// Native
+import * as fs from 'fs';
+import * as path from 'path';
+
 // Packages
 import * as winston from 'winston';
+import * as Transport from 'winston-transport';
+
+const logDrive = '/mnt/sda1';
+const logPath = path.join(logDrive, 'tally-lights.log');
 
 export function createLogger(label: string) {
 	const format = winston.format.combine(
@@ -15,17 +23,20 @@ export function createLogger(label: string) {
 		})
 	);
 
-	return winston.createLogger({
-		transports: [
-			new (winston.transports.Console)({
-				format,
-				level: 'debug'
-			}),
-			new winston.transports.File({
-				format,
-				level: 'debug',
-				filename: '/root/tally-lights.log'
-			})
-		]
-	});
+	const transports = [
+		new (winston.transports.Console)({
+			format,
+			level: 'debug'
+		})
+	] as Transport[];
+
+	if (fs.existsSync(logDrive)) {
+		transports.push(new winston.transports.File({
+			format,
+			level: 'debug',
+			filename: logPath
+		}));
+	}
+
+	return winston.createLogger({transports});
 }
