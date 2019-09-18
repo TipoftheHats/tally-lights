a5cedcee9409bdcd8307f7235a1a588095de49d4
Alex Van Camp <vancamp.alex@gmail.com>
2019-09-18 12:55:36 -0500

    fix(logger): fix file logs not working as intended
    The previous code would make a new file handle _for each logger instance_, which was bad and could have been deadlocking things.

--- src/logger.ts
+++ src/logger.ts
@@ -1,5 +1,3 @@
-'use strict';
-
 // Native
 import * as fs from 'fs';
 import * as path from 'path';
@@ -11,35 +9,54 @@ import * as Transport from 'winston-transport';
 const logDrive = '/root';
 const logPath = path.join(logDrive, 'tally-lights.log');
 
-export function createLogger(label: string) {
-	const format = winston.format.combine(
-		winston.format.colorize(),
-		winston.format.label({label}),
-		winston.format.timestamp(),
-		winston.format.splat(),
-		winston.format.prettyPrint(),
-		winston.format.printf(info => {
-			return `${info.timestamp} [${info.label}] ${info.level}: ${info.message}`;
-		})
-	);
+const defaultFormats = [
+	winston.format.timestamp(),
+	winston.format.splat(),
+	winston.format.prettyPrint(),
+	winston.format.printf(info => {
+		return `${info.timestamp} ${info.level}: ${info.message}`;
+	}),
+];
 
-	const transports = [
-		new (winston.transports.Console)({
-			format,
-			level: 'debug'
-		})
-	] as Transport[];
+const transports: Transport[] = [
+	new winston.transports.Console({
+		format: winston.format.combine(
+			winston.format.colorize(),
+			...defaultFormats,
+		),
+		level: 'info',
+	}),
+];
 
-	if (fs.existsSync(logDrive)) {
-		transports.push(new winston.transports.File({
-			format,
-			level: 'debug',
-			filename: logPath,
-			maxsize: 1000000, // 1MB
-			tailable: true,
-			maxFiles: 5
-		}));
-	}
+if (fs.existsSync(logDrive)) {
+	transports.push(new winston.transports.File({
+		format: winston.format.combine(...defaultFormats),
+		level: 'debug',
+		filename: logPath,
+		maxsize: 1000000, // 1MB
+		maxFiles: 5,
+		tailable: true,
+	}));
+}
 
-	return winston.createLogger({transports});
+transports.forEach(transport => {
+	transport.setMaxListeners(100);
+});
+
+export function createLogger(label: string): winston.Logger {
+	const logger = winston.createLogger({transports});
+	logger.setMaxListeners(100);
+	return new Proxy(logger, {
+		get(target, propName) {
+			const prop = target[propName as keyof winston.Logger];
+			if (typeof prop === 'function') {
+				return (...args: any[]) => {
+					args[0] = `[${label}] ${args[0]}`;
+					return (prop as any)(...args);
+				};
+			}
+
+			return prop;
+		},
+	});
 }
